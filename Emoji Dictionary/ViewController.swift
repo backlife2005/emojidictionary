3e41c2851d315a6af052477ac659e8658dfa485e
Mohammed Al-Khonaizi <backlife2005@gmail.com>
2017-02-10 12:38:31 +0300

    Added a Segway Connection to link the Table View with the View Controller of the 2nd Screen.

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -36,6 +36,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveSegway", sender: "👮")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
