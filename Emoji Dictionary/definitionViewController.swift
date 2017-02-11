21827e1d916ed02726dd9316b64c1ab1958ef7ee
Mohammed Al-Khonaizi <backlife2005@gmail.com>
2017-02-11 07:28:26 +0300

    Added an emoji variable which will hold the value of selected emoji from the TableView Controller later on.
    As of now, emoji is set to “NO EMOJI” string which is printed the definitionViewController is shown.

--- Emoji Dictionary/definitionViewController.swift
+++ Emoji Dictionary/definitionViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 
 class definitionViewController: UIViewController {
+    
+    var emoji = "NO EMOJI"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        print(emoji)
     }
 
     override func didReceiveMemoryWarning() {
